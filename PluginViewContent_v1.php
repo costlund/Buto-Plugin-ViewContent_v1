d6a14db510453594eca30f59011aac39d477d0fc
Claes Östlund <claes@stenbergit.se>
2019-09-27 17:02:11 +0200

    Add manifest.

--- PluginViewContent_v1.php
+++ PluginViewContent_v1.php
@@ -1,7 +1,6 @@
 <?php
 class PluginViewContent_v1{
   public function widget_include(){
-    wfPlugin::includeonce('wf/embed');
     $element = array();
     $element[] = wfDocument::createHtmlElement('style', ".plugin_view_content{display:none;}");
     $element[] = wfDocument::createWidget('wf/embed', 'embed', array('type' => 'script', 'file' => '/plugin/view/content_v1/js/function.js'));
