77ec2e50a55191df901ff937e68205835b5ff9a3
Claes Östlund <claes@stenbergit.se>
2019-09-30 11:24:41 +0200

    Add plugin wf/ajax to manifest.

--- PluginViewContent_v1.php
+++ PluginViewContent_v1.php
@@ -1,6 +1,7 @@
 <?php
 class PluginViewContent_v1{
   public function widget_include(){
+    wfPlugin::enable('wf/embed');
     $element = array();
     $element[] = wfDocument::createHtmlElement('style', ".plugin_view_content{display:none;}");
     $element[] = wfDocument::createWidget('wf/embed', 'embed', array('type' => 'script', 'file' => '/plugin/view/content_v1/js/function.js'));
